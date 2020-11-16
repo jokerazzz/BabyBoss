2c7114a8f27b312a57506652ab28f6298f5dff49
jokerazzz <youiever.ao@gmail.com>
2020-11-16 23:29:19 +0600

    fixed

--- js/main.js
+++ js/main.js
@@ -3,26 +3,50 @@ new WOW().init();
 
 $('.mobile-btn').on('click', function(){
     $('.mobile-menu ul').toggleClass('active');
-})
+});
 
 $('.filter2').on('click', function() {
   $('.grid').toggleClass('active2');
   $('.grid').removeClass('active3');
-})
+});
 
 $('.filter3').on('click', function() {
 $('.grid').toggleClass('active3');
 $('.grid').removeClass('active2');
-})
+});
+
+$('.filter').on('click', function() {
+  setTimeout(() => {
+    $('.filter__menu').toggleClass('active');
+  }, 0)
+});
+
+$(document).on('click', function(event) {
+  if ($('.filter__menu').hasClass('active') && !event.target.closest('.filter__menu')) {
+    $('.filter__menu').removeClass('active');
+  }
+});
+
+  
 
 $('.filter1').on('click', function() {
 $('.grid').removeClass('active3');
 $('.grid').removeClass('active2');
-})
+});
 
+$(function(){
+    $('input[type="number"]').niceNumber();
+  
+  });
+  
 
-
-
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 690,
+  max: 89900,
+  from: 0,
+  to: 50000,
+});
 
 
 
